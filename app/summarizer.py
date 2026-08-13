2b8066096a0bd172077bad525b109369910889cd
Ayushi Singh <singh9ayushi9@gmail.com>
2026-08-13 21:49:51 +0200

    fixes

--- app/summarizer.py
+++ app/summarizer.py
@@ -1,17 +1,27 @@
 import streamlit as st
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 @st.cache_resource
 def load_summarizer():
-    return pipeline(
-        "summarization",
-        model="sshleifer/distilbart-cnn-12-6"
-    )
+    model_name = "sshleifer/distilbart-cnn-12-6"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    return tokenizer, model
 
-def summarize_text(chunks, summarizer):
+def summarize_text(chunks, summarizer_tuple):
+    tokenizer, model = summarizer_tuple
     summaries = []
+    
     for chunk in chunks:
-        # Generate summary per text chunk
-        res = summarizer(chunk, max_length=130, min_length=30, do_sample=False)
-        summaries.append(res[0]['summary_text'])
+        inputs = tokenizer(chunk, return_tensors="pt", max_length=1024, truncation=True)
+        summary_ids = model.generate(
+            inputs["input_ids"], 
+            max_length=130, 
+            min_length=30, 
+            length_penalty=2.0, 
+            num_beams=2
+        )
+        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        summaries.append(summary)
+        
     return " ".join(summaries)
